84a614ba1465ccc9224e53164e5f2ce27863bd26
Juan Pablo Addeo <jpaddeo@gmail.com>
2023-05-08 22:11:07 -0300

    fix: :bug: changin class by className

--- src/components/icons/LockIcon.jsx
+++ src/components/icons/LockIcon.jsx
@@ -10,7 +10,7 @@ export default function LockIcon({ className }) {
       strokeWidth='2'
       strokeLinecap='round'
       strokeLinejoin='round'
-      class={`feather feather-lock ${className}`}
+      className={`feather feather-lock ${className}`}
     >
       <rect x='3' y='11' width='18' height='11' rx='2' ry='2'></rect>
       <path d='M7 11V7a5 5 0 0 1 10 0v4'></path>
